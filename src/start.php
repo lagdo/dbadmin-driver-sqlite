51b7e4957b2ee8d4713c846606127a993762bca5
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-09-02 06:34:55 +0200

    Use alias in di definitions.

--- src/start.php
+++ src/start.php
@@ -1,8 +1,10 @@
 <?php
 
+$di = \jaxon()->di();
 // Register the database classes in the dependency container
-\jaxon()->di()->set('adminer_server_sqlite', function($di) {
+$di->set(Lagdo\DbAdmin\Driver\Sqlite\Server::class, function($di) {
     return new Lagdo\DbAdmin\Driver\Sqlite\Server(
         $di->get(Lagdo\Adminer\Driver\DbInterface::class),
         $di->get(Lagdo\Adminer\Driver\UtilInterface::class));
 });
+$di->alias('adminer_server_sqlite', Lagdo\DbAdmin\Driver\Sqlite\Server::class);
