dacf8e1161e6b83401360df50e0a222709e95f88
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-09-02 11:08:41 +0200

    Improved DI definitions.

--- src/start.php
+++ src/start.php
@@ -2,9 +2,5 @@
 
 $di = \jaxon()->di();
 // Register the database classes in the dependency container
-$di->set(Lagdo\DbAdmin\Driver\Sqlite\Server::class, function($di) {
-    return new Lagdo\DbAdmin\Driver\Sqlite\Server(
-        $di->get(Lagdo\Adminer\Driver\DbInterface::class),
-        $di->get(Lagdo\Adminer\Driver\UtilInterface::class));
-});
+$di->auto(Lagdo\DbAdmin\Driver\Sqlite\Server::class);
 $di->alias('adminer_server_sqlite', Lagdo\DbAdmin\Driver\Sqlite\Server::class);
