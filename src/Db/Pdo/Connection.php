3c706c68afedd5babe9a7389f11c1cc9e5bd9ccc
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-10-27 02:32:49 +0200

    Fixed errors revealed by Scrutinizer.

--- src/Db/Pdo/Connection.php
+++ src/Db/Pdo/Connection.php
@@ -9,16 +9,6 @@ class Connection extends PdoConnection
 {
     use ConfigTrait;
 
-    public function multiQuery(string $query)
-    {
-        return $this->result = $this->query($query);
-    }
-
-    public function nextResult()
-    {
-        return false;
-    }
-
     /**
      * @inheritDoc
      */
